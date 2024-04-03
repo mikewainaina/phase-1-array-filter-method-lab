41db09aae1ef48b07c61a5b98ccf41aca21425c9
Michael <michael.wainaina@student.moringaschool.com>
2024-04-03 18:53:01 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+// index.js
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
